dee7e2d70f08362fa8016ca20b52a89f70bc3388
JoshStaff99 <Joshua.Stafford@netmatters-scs.com>
2025-01-03 14:59:37 +0000

    Image Generation Function and file clear up
    Implemented the random image generation function using jQuery. The function can be used an unlimited amount of times and generates an image at random with set parameters for the image resolution. Cleared up the JavaScript file to be more readable.

--- js/main.js
+++ js/main.js
@@ -2,25 +2,36 @@
 // JavaScript
 // ==========================================================================
 
-// Email Vaildator
-// Variables
+////////
+// Global Variables
+////////
+
+// Image Generation Variables
+const picsumSeed = "https://picsum.photos/seed/";
+const imageResolution = "/300/400";
+
+// Email Variables
 const $form_EmailAddress = $('#emailAd');
 let isValidEmailAddress = ()=> /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test($form_EmailAddress.val());
 
+
+//////////
+// Email Vaildator
+/////////
 // Checks if the email field is empty or invalid. Show's error alerts when needed
 function validateForm() {
-    let emailaddressForm = document.forms["contactForm"]["emailaddress"].value;
+  let emailaddressForm = document.forms["contactForm"]["emailaddress"].value;
   
-    if (emailaddressForm == "") {
-      alert("Email Address must be filled out");
-     return false; 
-    } else if (!isValidEmailAddress()) {
-      alert("Not a valid email address");
-      return false;
-    } else {
-      return true;
-    }
+  if (emailaddressForm == "") {
+    alert("Email Address must be filled out");
+    return false; 
+  } else if (!isValidEmailAddress()) {
+    alert("Not a valid email address");
+    return false;
+  } else {
+    return true;
   }
+}
   
   // Prevents the page from refreshing when you click on the submit button
   const $submitBtn = $('.btn-submit');
@@ -31,4 +42,19 @@ function validateForm() {
       $('#contactFormSubmit')[0].reset();
     } else
     event.preventDefault();
-  });
+});
+
+////////
+// Image Generation
+////////
+
+$(document).ready(function () {
+  const randomImage = document.getElementById('random-image');
+  const randomSeed = Math.floor(Math.random() * 1000);
+
+  randomImage.src = `${picsumSeed}${randomSeed}${imageResolution}`;
+      $('.generate-image-btn').click(function() {
+          const randomSeed = Math.floor(Math.random() * 1000);
+          randomImage.src = `${picsumSeed}${randomSeed}${imageResolution}`;
+      });
+});
